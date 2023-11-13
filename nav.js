d27c86496ebcc9a000e9276bc8336c1faf5929ee
techla2019 <114215025+techla2019@users.noreply.github.com>
2023-11-13 22:49:29 +0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,5 @@
+const addw = document.getElementsByClassName('humbaga')[0];
+const nav =document.getElementsByClassName('addi')[0];
+addw.addEventListener('click', ()=>{
+    nav.classList.toggle('active')
+})
