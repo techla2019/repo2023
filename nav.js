3cb8d112a1cf07c15b454f5959b99cefee274488
evans <techlacollege@gmail.com>
2023-11-21 23:28:02 +0300

    a cartain added

--- nav.js
+++ nav.js
@@ -3,3 +3,9 @@ const nav =document.getElementsByClassName('addi')[0];
 addw.addEventListener('click', ()=>{
     nav.classList.toggle('active')
 })
+
+const addd = document.getElementsByClassName('humbaga')[0];
+const na =document.getElementsByClassName('transparent')[0];
+addd.addEventListener('click', ()=>{
+    na.classList.toggle('ctive')
+})
