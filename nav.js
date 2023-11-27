93f5028dc0dbeab34deb6e572fe59a188d9d719d
evans <techlacollege@gmail.com>
2023-11-27 19:41:22 +0300

    box shandow

--- nav.js
+++ nav.js
@@ -1,11 +1,12 @@
-const addw = document.getElementsByClassName('humbaga')[0];
-const nav =document.getElementsByClassName('addi')[0];
-addw.addEventListener('click', ()=>{
-    nav.classList.toggle('active')
-})
+// const addw = document.getElementsByClassName('humbaga')[0];
+// const nav =document.getElementsByClassName('addi')[0];
+// addw.addEventListener('click', ()=>{
+//     nav.classList.toggle('active')
+// })
 
 const addd = document.getElementsByClassName('humbaga')[0];
 const na =document.getElementsByClassName('transparent')[0];
 addd.addEventListener('click', ()=>{
     na.classList.toggle('ctive')
 })
+
